1ce20eaf7ba2b70052e3a3a50a3d47f3f7291be2
LLL2410 <l332962812@163.com>
2020-08-19 22:30:00 +0800

    first commit

---
+++ src/main/java/com/shjhyj/ShjhyjApplication.java
@@ -0,0 +1,13 @@
+package com.shjhyj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShjhyjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShjhyjApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shjhyj/ShjhyjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shjhyj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShjhyjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
